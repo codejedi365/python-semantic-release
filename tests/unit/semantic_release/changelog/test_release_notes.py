0ea8c3e84040f05039a2d11af9d11c3633c2fa78
codejedi365 <codejedi365@gmail.com>
2024-12-27 23:13:47 -0500

    test(release-notes): update unit tests to include license statement

--- tests/unit/semantic_release/changelog/test_release_notes.py
+++ tests/unit/semantic_release/changelog/test_release_notes.py
@@ -28,10 +28,12 @@ def release_notes_template() -> str:
 
 
 @pytest.mark.parametrize("mask_initial_release", [True, False])
+@pytest.mark.parametrize("license_name", ["", "MIT"])
 @pytest.mark.parametrize("hvcs_client", [Github, Gitlab, Gitea, Bitbucket])
 def test_default_release_notes_template(
     example_git_https_url: str,
     hvcs_client: type[Github | Gitlab | Gitea | Bitbucket],
+    license_name: str,
     artificial_release_history: ReleaseHistory,
     mask_initial_release: bool,
     today_date_str: str,
@@ -72,7 +74,15 @@ def test_default_release_notes_template(
         os.linesep,
         [
             f"## v{version} ({today_date_str})",
-            "",
+            *(
+                [""]
+                if not license_name
+                else [
+                    "",
+                    f"_This release is published under the {license_name} License._",
+                    "",
+                ]
+            ),
             "### Feature",
             "",
             "- {commit_scope}{commit_desc} ([`{short_hash}`]({url}))".format(
@@ -144,6 +154,7 @@ def test_default_release_notes_template(
         history=artificial_release_history,
         style="angular",
         mask_initial_release=mask_initial_release,
+        license_name=license_name,
     )
 
     assert expected_content == actual_content
@@ -361,10 +372,12 @@ def test_default_release_notes_template_w_multiple_brk_changes(
     assert expected_content == actual_content
 
 
+@pytest.mark.parametrize("license_name", ["", "MIT"])
 @pytest.mark.parametrize("hvcs_client", [Github, Gitlab, Gitea, Bitbucket])
 def test_default_release_notes_template_first_release_masked(
     example_git_https_url: str,
     hvcs_client: type[Bitbucket | Gitea | Github | Gitlab],
+    license_name: str,
     single_release_history: ReleaseHistory,
     today_date_str: str,
 ):
@@ -381,7 +394,15 @@ def test_default_release_notes_template_first_release_masked(
         os.linesep,
         [
             f"## v{version} ({today_date_str})",
-            "",
+            *(
+                [""]
+                if not license_name
+                else [
+                    "",
+                    f"_This release is published under the {license_name} License._",
+                    "",
+                ]
+            ),
             "- Initial Release",
             "",
         ],
@@ -394,15 +415,18 @@ def test_default_release_notes_template_first_release_masked(
         history=single_release_history,
         style="angular",
         mask_initial_release=True,
+        license_name=license_name,
     )
 
     assert expected_content == actual_content
 
 
+@pytest.mark.parametrize("license_name", ["", "MIT"])
 @pytest.mark.parametrize("hvcs_client", [Github, Gitlab, Gitea, Bitbucket])
 def test_default_release_notes_template_first_release_unmasked(
     example_git_https_url: str,
     hvcs_client: type[Bitbucket | Gitea | Github | Gitlab],
+    license_name: str,
     single_release_history: ReleaseHistory,
     today_date_str: str,
 ):
@@ -425,7 +449,15 @@ def test_default_release_notes_template_first_release_unmasked(
         os.linesep,
         [
             f"## v{version} ({today_date_str})",
-            "",
+            *(
+                [""]
+                if not license_name
+                else [
+                    "",
+                    f"_This release is published under the {license_name} License._",
+                    "",
+                ]
+            ),
             "### Feature",
             "",
             "- {commit_scope}{commit_desc} ([`{short_hash}`]({url}))".format(
@@ -447,6 +479,7 @@ def test_default_release_notes_template_first_release_unmasked(
         history=single_release_history,
         style="angular",
         mask_initial_release=False,
+        license_name=license_name,
     )
 
     assert expected_content == actual_content
