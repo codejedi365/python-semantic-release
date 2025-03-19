c6bb278e85fc316c5dcdffe58ddcf49a43474227
codejedi365 <codejedi365@gmail.com>
2025-03-18 22:39:29 -0600

    chore: new testing repos -- temporary

--- tests/fixtures/repos/__init__.py
+++ tests/fixtures/repos/__init__.py
@@ -1,4 +1,6 @@
 from tests.fixtures.repos.git_flow import *
 from tests.fixtures.repos.github_flow import *
+from tests.fixtures.repos.monorepo import *
+from tests.fixtures.repos.release_flow import *
 from tests.fixtures.repos.repo_initial_commit import *
 from tests.fixtures.repos.trunk_based_dev import *
